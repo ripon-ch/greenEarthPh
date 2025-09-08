7f48ea1c7afb4deb4975d0df1ae56a7ac637b874
Project <jalalchowdhury47@gmail.com>
2025-09-08 12:20:41 +0400

    card section

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+const API = {
+    allPlants: "https://openapi.programming-hero.com/api/plants",
+    categories: "https://openapi.programming-hero.com/api/categories",
+    byCategory: id => `https://openapi.programming-hero.com/api/category/${id}`,
+    detail: id => `https://openapi.programming-hero.com/api/plant/${id}`
+};
